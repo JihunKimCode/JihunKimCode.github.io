ad38e524d52ab637ec6ee2e6b097acfb9bccc21c
JihunKimCode <jiunkim8@gmail.com>
2023-12-14 01:41:25 -0600

    collapsible block

--- script.js
+++ script.js
@@ -248,3 +248,18 @@ function darkmode() {
     // Call header color change function after toggling dark mode
     handleHeaderColorChange();
 } 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
+    }
+  });
+}
