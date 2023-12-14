92f020381c39c083f26344019119aefccbf1cb9a
JihunKimCode <jiunkim8@gmail.com>
2023-12-14 13:03:59 -0600

    collapsible, buttons, and resume

--- script.js
+++ script.js
@@ -249,10 +249,12 @@ function darkmode() {
     handleHeaderColorChange();
 } 
 
+/*************************
+ *  Collapsible Content  *
+ *************************/
 var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
+for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
